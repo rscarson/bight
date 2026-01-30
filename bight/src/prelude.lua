68aa23619d6ffefe87f4c1a2f15df5232053a937
WASDetchan <yarik.antipov2008@gmail.com>
2026-01-30 20:43:35 +0300

    Excel-style math functions

--- bight/src/prelude.lua
+++ bight/src/prelude.lua
@@ -16,3 +16,35 @@ setmetatable(_G, {
     return CELL_POS(index)
   end
 })
+
+SIN = math.sin
+COS = math.cos
+TG = math.tan
+
+function CTG(x)
+  return COS(x) / SIN(x)
+end
+
+TAN = TG
+COT = CTG
+
+ASIN = math.asin
+ACOS = math.acos
+ATAN = math.atan
+
+LOG = math.log
+
+function LN(x)
+  return math.log(x)
+end
+
+function LOG2(x)
+  return math.log(x, 2)
+end
+
+LOG10 = math.log10
+LG = math.log10
+
+SQRT = math.sqrt
+POW = math.pow
+EXP = math.exp
